9f1a2497837a9a3e9f699d9a2a0f181a295fd0db
Leonardo Almeida <leo.almeida.silva@hotmail.com>
2020-04-14 20:22:28 -0300

    escalando com workers

--- email-worker-compose/worker/worker.py
+++ email-worker-compose/worker/worker.py
@@ -7,7 +7,7 @@ from random import randint
 if __name__ == '__main__':
     # redis_host = os.getenv('REDIS_HOST', 'queue')
     r = redis.Redis(host='queue', port=6379, db=0)
-    # print('Aguardando mensagens ...')
+    print('Aguardando mensagens ...')
     while True:
         mensagem = json.loads(r.blpop('sender')[1])
         # Simulando envio de e-mail...
