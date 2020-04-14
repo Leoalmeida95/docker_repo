c030762754a6285e6e4be7a28b44da169f4079a2
Leonardo Almeida <leo.almeida.silva@hotmail.com>
2020-04-14 20:41:44 -0300

    add enviroments

--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -10,15 +10,15 @@ class Sender(Bottle):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
         
-        # redis_host = os.getenv('REDIS_HOST', 'queue')
-        self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
-        DSN = 'host=db dbname=email_sender user=postgres password=postgres'
+        redis_host = os.getenv('REDIS_HOST', 'queue')
+        self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
 
-        # db_host = os.getenv('DB_HOST', 'db')
-        # db_user = os.getenv('DB_USER', 'postgres')
-        # db_name = os.getenv('DB_NAME', 'sender')
-        # dsn = f'dbname={db_name} user={db_user} host={db_host}'
-        self.conn = psycopg2.connect(DSN)
+        db_host = os.getenv('DB_HOST', 'db')
+        db_user = os.getenv('DB_USER', 'postgres')
+        db_name = os.getenv('DB_NAME', 'sender')
+        db_password = os.getenv('DB_PASSWORD', 'postgres')
+        dsn = f'dbname={db_name} user={db_user} host={db_host} password={db_password}'
+        self.conn = psycopg2.connect(dsn)
         
     def register_message(self, assunto, mensagem):
         SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
